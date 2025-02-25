e718be452ea2a73b7d7e9685dc81954bc325e286
taeseokyang <ts.yang.0123@gmail.com>
2025-02-25 16:06:59 +0900

    feat

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,19 +12,14 @@ const Layout = styled.div`
     left: 0;
     width: 100%;
     /* height:100px; */
+    
     background: #eeeeee;
 
 `;
 const Content = styled.div`
-  padding: 100px 20px 20px 20px;
+  padding: 100px 20px 10px 20px;
   max-width: 1000px;
   margin: 0px auto;
-  /* text-align: center; */
-  display: flex;
-  justify-content: center;
-    /* align-items: center;  */
-    flex-direction: row;
-    gap: 15px;
 `;
 const Item = styled.div`
   font-size: 13px;
@@ -32,10 +27,18 @@ const Item = styled.div`
   white-space: nowrap;
 `;
 
+const Line = styled.div`
+display: flex;
+    flex-direction: row;
+    gap: 15px;
+    margin-bottom: 10px;
+`;
+
 const ClickItem = styled.div`
   font-size: 13px;
   color: #5c5c5c;
   white-space: nowrap;
+  border-bottom: 1px solid #eeeeee;
   &:hover{
       border-bottom: 1px solid #5c5c5c;
     }
@@ -46,13 +49,22 @@ const Footer = () => {
     <Container>
       <Layout>
           <Content>
-            <Item>ⓒ 2025 The Gachon Herald</Item>
+            <Line>
             <Link to={"/contactus"}>
             <ClickItem>Contact Us</ClickItem>
             </Link>
             <Link to={"/aboutus"}>
             <ClickItem>About Us</ClickItem>
             </Link>
+            </Line>
+           
+            <Line style={{justifyContent: "space-between"}}>
+            <Item>ⓒ 2025 The Gachon Herald</Item>
+            <a href={"https://yangsoft.netlify.app"}>
+            <ClickItem >POWERED BY YANGSOFT</ClickItem>
+            </a>
+            </Line>
+         
             
           </Content>
       </Layout>
